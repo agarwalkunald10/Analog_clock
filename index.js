3be953f3fea284597ba10873c916b348e976c5a0
Kunal Agarwal <67964845+agarwalkunald10@users.noreply.github.com>
2022-04-10 12:06:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+
+    //alert("" + htime + ":" + mtime + ":" + stime);
+
+    document.getElementById('hour').style.transform = "rotate(" + hrotation + "deg)";
+    document.getElementById('minute').style.transform = "rotate(" + mrotation + "deg)";
+    document.getElementById('second').style.transform = "rotate(" + srotation + "deg)";
+}, 1000);
